4da640e2a584faabdcd6f61bbab727c23e654f87
Creative Element <creativelemet@gmail.com>
2018-12-17 19:42:30 +0100

    Create Student.java
    Student class in Java

---
+++ Student.java
@@ -0,0 +1,44 @@
+/**
+ * Student
+ */
+public class Student {
+    private int id;
+    private String name, lastname;
+    private String password;
+    public Student () {
+        this(0,"","","");
+    }
+
+    public Student(int id, String name, String lastname, String password)
+    {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.password = password;
+    }
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public String getLastname()
+    {
+        return this.lastname;
+    }
+
+    public String getPassword()
+    {
+        return this.password;
+    }
+}
